3a7a122809ce3dcd72e0cad33afe5515a12b7a25
EdASalazar <edasalazar123@gmail.com>
2023-02-27 19:23:03 -0800

    Got most of it done

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+finches = [
+    {'name': 'Gibson', 'bridge_pickup': 'humbucker'},
+    {'name': 'Strat', 'bridge_pickup': 'single coil'},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-    
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+    'finches': finches
+})
